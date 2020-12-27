919d465e18072efa026ba6f9505b97d4b3da048b
Marcus Teixeira <40602844+marcusteixeira@users.noreply.github.com>
2020-12-27 22:03:02 +0000

    Initial Commit

---
+++ aprenda-go/cap8/cap8.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	times := []string{"Flamengo", "Vasco", "Botafogo", "Fluminense"}
+	fmt.Println(times)
+}
---
+++ go-exercises/hello_world.go
@@ -0,0 +1,11 @@
+// https://golangr.com/hello-world/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("My name is Marcus")
+	fmt.Println("My address is 127.0.0.1")
+}
